5f4754312c8b1c529e6b938abf2ba71fcac15ebe
davur <davur@davur.net>
2024-01-30 08:59:10 +1100

    Added --interactive mode

--- lesh/__init__.py
+++ lesh/__init__.py
@@ -3,11 +3,10 @@
 """lesh helps filter, sort and display tabular data
 
 Usage:
-  lesh.py --file=FILE --select=NAME,AGE
-  lesh.py --select NAME,AGE
-  lesh.py --select NAME,AGE --where STATUS=Pending --order-by NAME
-  lesh.py [options]
-  lesh.py [--help | --version]
+  lesh [--interactive] [--file=FILE] [--select=COL1,COL2] [--where=CONDITION]
+  lesh --select NAME,AGE --where STATUS=Pending --order-by NAME
+  lesh [options]
+  lesh [--help | --version]
 
 Options:
   -h, --help             Show this screen.
@@ -27,18 +26,18 @@ import utils
 
 import sys
 import curses
-from curses import wrapper
-
 
 class Lesh:
 
     headerline = None
     lines = None
     columns = None  # dict of column_name => (start, end) character indices
+    rows = None
 
     select_columns = None
+    where = None
 
-    interactive = False
+    is_interactive = False
 
     def load_infile(self, infile):
         lines = None
@@ -68,16 +67,26 @@ class Lesh:
             else:
                 stop = len(self.headerline)
 
-            columns[c] = (start, stop)
+            columns[c] = (len(columns), start, stop)
             start = stop
 
         # Get last column width from data
         max_length = start
         for line in lines:
             max_length = max(max_length, len(line))
-        columns[c] = (columns[c][0], max_length)
+        columns[c] = (columns[c][0], columns[c][1], max_length)
 
         self.columns = columns
+
+        rows = []
+        for line in lines:
+            row = []
+            for col in columns:
+                _, start, stop = columns[col]
+                row.append(line[start:stop].strip())
+            rows.append(row)
+        self.rows = rows
+
 
     def main(self):
         arguments = docopt(str(__doc__), version="0.0.1")
@@ -90,20 +99,284 @@ class Lesh:
         filename = arguments['--file']
         self.load_infile(filename)
 
-        self.interactive = arguments['--interactive']
+        self.is_interactive = arguments['--interactive']
 
         self.select_columns = arguments['--select']
         if self.select_columns == 'ALL':
-            self.select_columns = None
+            self.select_columns = list(self.columns.keys())
         else:
             self.select_columns = [c.strip()
                                    for c in self.select_columns.split(',')]
+        where = arguments['--where']
+        if where:
+            parts = where.split("=", 1)
+            if len(parts) == 2:
+                col, val = parts
+                if col not in self.columns:
+                    print("Unknown column '{col}' found in --where")
+                    exit()
+                self.where = (col, val)
 
-        self.print()
+        if self.is_interactive:
+            curses.wrapper(self.interactive)
+        else:
+            self.table()
+
+    def interactive(self, w):
+
+        y = 0
+        selected = 0
+
+        if curses.has_colors():
+            curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
+            curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
+            curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_CYAN)
+
+        while True:
+            w.clear()
+            self.table(w.addstr)
+
+            ch = w.getch()
+            if ch == ord('q'): # q
+                break
+            if ch == ord('s'):
+                self.select_prompt(w)
+            elif ch == ord('w'):
+                self.where_prompt(w)
+            else:
+                s = f'{ch=}'
+                self.statusline(w, s)
+
+
+    def all_columns(self):
+        cols = list(self.select_columns)
+        for col in self.columns.keys():
+            if col not in cols:
+                cols.append(col)
+        return cols
+
+    def statusline(self, w, msg):
+        rows, cols = w.getmaxyx()
+        w.addstr(rows - 2, 0, f'{msg: <{cols}}', curses.A_STANDOUT)
+        # TODO look into why just rows throws error, but rows - 2 doesn't
+        #   File "/Users/davur/repos/davur/lesh/lesh/__init__.py", line 130, in interactive
+        # self.select_prompt(w)
+        # File "/Users/davur/repos/davur/lesh/lesh/__init__.py", line 206, in select_prompt
+        # self.statusline(w, msg)
+        # File "/Users/davur/repos/davur/lesh/lesh/__init__.py", line 150, in statusline
+        # w.addstr(68, 0, f'{msg: <{cols}}', curses.A_STANDOUT)
+        # _curses.error: addwstr() returned ERR
+
+
+    def where_prompt(self, w):
+        y = 0
+        x = 0
+        selected_col = 0
+        selected_val = 0
+
+        all_columns = self.all_columns()
+        distinct_values = [] # ['Ready', 'Cordoned', 'NotReady']
+
+        while True:
+            y = 0
+            w.clear()
+            w.refresh()
+            col = all_columns[selected_col]
+            if x == 1:
+                val = distinct_values[selected_val]
+            else:
+                val = ""
+
+            if x == 0:
+                w.addstr(0, 0, "Choose a column: ", curses.A_BOLD)
+            else:
+                w.addstr(0, 0, "Choose a value: ", curses.A_BOLD)
+
+            msg = f"--where='{col}={val}'"
+            self.statusline(w, msg)
+
+            for col in all_columns:
+                attr = 0
+                if y < len(self.select_columns):
+                    attr |= curses.A_BOLD
+                    prefix = ' '
+                else:
+                    attr |= curses.A_DIM
+                    prefix = ' '
+                if y == selected_col:
+                    attr |= curses.color_pair(1)
+                s = f'{prefix}{col: <20}'
+                w.addstr(2 + y, 0, s, attr)
+                y += 1
+
+            if x == 1:
+                y = 0
+                for val in distinct_values:
+                    attr = 0
+                    if y == selected_val:
+                        attr |= curses.color_pair(1)
+                    s = f'{val: <20}'
+                    w.addstr(2 + y, 21, s, attr)
+                    y += 1
+
+
+            ch = w.getch()
+
+            # Move cursor
+            if ch == 258 or ch == 106: # down
+                if x == 0:
+                    selected_col += 1
+                else:
+                    selected_val += 1
+            elif ch == 259 or ch == 107: # up
+                if x == 0:
+                    selected_col -= 1
+                else:
+                    selected_val -= 1
+
+            if x == 0:
+                selected_col %= len(self.columns)
+            else:
+                selected_val %= len(distinct_values)
+
+            if x == 0 and ch == ord('l'):
+                x += 1
+                distinct_values = self.distinct_values(all_columns[selected_col])
+            elif x == 1 and ch == ord('h'):
+                x -= 1
+
+            # Back to table
+            if ch == 10: # enter
+                if x == 1:
+                    self.where = (all_columns[selected_col], distinct_values[selected_val])
+                return
+
+
+    def select_prompt(self, w):
+        y = 0
+        selected = 0
+        while True:
+            y = 0
+
+            w.clear()
+            w.refresh()
+            all_columns = self.all_columns()
+
+            w.addstr(0, 0, "Choose your columns: ", curses.A_BOLD)
+            msg = "--select=%s" % (",".join(self.select_columns))
+            self.statusline(w, msg)
+            # w.addstr(1, 0, "-" * len(header))
+
+            for col in all_columns:
+                attr = 0
+                if y < len(self.select_columns):
+                    attr |= curses.A_BOLD
+                    prefix = ' '
+                else:
+                    attr |= curses.A_DIM
+                    prefix = ' '
+                if y == selected:
+                    attr |= curses.color_pair(1)
+                s = f'{prefix}{col: <20}'
+                w.addstr(2+y, 0, s, attr)
+                y += 1
+
+            ch = w.getch()
+
+            # Move cursor
+            if ch == 258 or ch == 106: # down
+                selected += 1
+            if ch == 259 or ch == 107: # up
+                selected -= 1
+            selected %= len(self.columns)
+
+            # Move column under cursor
+            if ch == 75 or ch == 104 or ch == 260: # move up
+                if 0 < selected < len(self.select_columns):
+                    col = self.select_columns[selected]
+                    self.select_columns.pop(selected)
+                    selected -= 1
+                    self.select_columns.insert(selected, col)
+            if ch == 74 or ch == 108 or ch == 261: # move down
+                if selected < len(self.select_columns) - 1:
+                    col = self.select_columns[selected]
+                    self.select_columns.pop(selected)
+                    selected += 1
+                    self.select_columns.insert(selected, col)
+
+            # Toggle column
+            if ch == 32: # space
+                if selected < len(self.select_columns):
+                    self.select_columns.pop(selected)
+                else:
+                    self.select_columns.append(all_columns[selected])
+
+            # Back to table
+            if ch == 10: # enter
+                return
+
+
+    def distinct_values(self, col):
+        col_index = self.columns[col][0]
+        values = set([row[col_index] for row in self.rows])
+        return list(values)
+
+
+    def table(self, write=None):
+        if not write:
+            write = sys.stdout.write
+        if not self.select_columns:
+            write(self.headerline)
+            write('\n')
+        else:
+            for c in self.select_columns:
+                _, start, stop = self.columns[c]
+                if len(self.headerline) > start:
+                    width = stop - start
+                    write(f'{self.headerline[start:stop]: <{width}}')
+            write('\n')
+
+        for row in self.rows:
+            if not self.select_columns:
+                write(line)
+                write('\n')
+                continue
+
+            if self.where:
+                col, val = self.where
+                if row[self.columns[col][0]] != val:
+                    continue
+
+            for c in self.select_columns:
+                index, start, stop = self.columns[c]
+                width = stop - start
+                write(f'{row[index]: <{width}}')
+            write('\n')
+
+#         for line in self.lines:
+#             if not self.select_columns:
+#                 write(line)
+#                 write('\n')
+#                 continue
+#
+#             # TODO: Parse table into data structure
+#             where = self.where
+#             if where:
+#                 col, val = where
+#                 start, stop = self.columns[col]
+#                 cur = line[start:stop]
+#                 if cur.strip() != val.strip():
+#                     continue
+#
+#             for c in self.select_columns:
+#                 start, stop = self.columns[c]
+#                 if len(line) > start:
+#                     width = stop - start
+#                     write(f'{line[start:stop]: <{width}}')
+#             write('\n')
 
 
     def print(self):
-
         if not self.select_columns:
             print(self.headerline)
         else:
@@ -126,9 +399,8 @@ class Lesh:
                     print(f'{line[start:stop]: <{width}}', end='')
             print()
 
-
 if __name__ == '__main__':
     lesh = Lesh()
     lesh.main()
+    # wrapper(main)
     # lesh.interactive()
-
